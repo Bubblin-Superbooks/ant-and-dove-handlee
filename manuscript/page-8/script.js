42647255e35298f0433292c1c9bec66150057a55
Craig Leat <craig.leat@gmail.com>
2016-02-25 11:53:33 +0200

    don't ask...

--- manuscript/page-8/script.js
+++ manuscript/page-8/script.js
@@ -11,7 +11,6 @@ function playAudio(id) {
   var someNoise = document.getElementById(id);
     someNoise.play();
 }
-
 function pauseAudio(id) {
   var someNoise = document.getElementById(id);
     someNoise.pause();
